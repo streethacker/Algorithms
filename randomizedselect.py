799e98e7bc20a93cad4c70f96e4fc4811083b27d
streethacker <silverbullet7714@126.com>
2013-12-21 20:46:32 +0800

    correct some mistakes in randomizedselect.py

--- randomizedselect.py
+++ randomizedselect.py
@@ -1,11 +1,11 @@
 #/usr/bin/python
 #-*- coding:utf-8 -*-
 
-from quicksort import Partition
+import quicksort
 
 def RandomizedSelect(a, p, r, k):
 	if p == r: return a[p]
-	i = Partition(a, p, r)
+	i = quicksort.RandomizedPartition(a, p, r)
 	j = i - p + 1
 	if k <= j:
 			return RandomizedSelect(a, p, i, k)
@@ -14,4 +14,4 @@ def RandomizedSelect(a, p, r, k):
 
 if __name__ == "__main__":
 	a = [11, 2, -8, 3, 100, 77, -3, 10, 35, 80, -2, 2]
-	print RandomizedSelect(a, 0, 11, 6)		
+	print RandomizedSelect(a, 0, 11, 1)		
